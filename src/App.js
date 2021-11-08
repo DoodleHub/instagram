c33108c3e86a54127e648bbf2c2309b8abcbdd6b
Chao Chen <chaochen@live.com>
2021-11-07 21:04:31 -0500

    created not found page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/sign-up'));
+const NotFound = lazy(() => import ('./pages/not-found'));
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login}/>
           <Route path={ROUTES.SIGN_UP} component={SignUp}/>
+          <Route path={ROUTES.NOT_FOUND} component={NotFound}/>
         </Switch>
       </Suspense>
     </Router>
